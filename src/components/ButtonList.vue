61d1eb8ed6045eecf679e5adf9932b693bcdafe4
MFukazawa <micahfukazawa@gmail.com>
2020-11-06 18:23:23 +0900

    [Update] Add Single Button, button List components

---
+++ src/components/ButtonList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex justify-center items-center">
+    <SingleButton v-for="i in 10" :key="i.index" text="asdfasdf" />
+  </div>
+</template>
+
+<script>
+import SingleButton from './SingleButton'
+
+export default {
+components: {
+  SingleButton
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/SingleButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="border border-black flex items-center justify-center p-6">
+    {{ text }}
+  </div>
+</template>
+
+<script>
+export default {
+props: {
+  text: {
+    type: String,
+    default: ''
+  }
+}
+}
+</script>
+
+<style>
+
+</style>
